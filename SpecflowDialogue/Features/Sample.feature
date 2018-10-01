dd2a1a99adb3896b9314a8dee348af1593d6fd31
ChrisLDone <40792960+ChrisLDone@users.noreply.github.com>
2018-10-01 19:52:53 +0100

    Added Specflow Dialogue

---
+++ SpecflowDialogue/Features/Sample.feature
@@ -0,0 +1,10 @@
+﻿Feature: Sample
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@bbchome
+Scenario: Sports Link Appears
+	Given I have gone to bbc
+	When The Home Page Loads
+	Then The Sport Link appears
---
+++ SpecflowDialogue/SampleSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SpecflowDialogue
+{
+    [Binding]
+    public class SampleSteps
+    {
+        [Given(@"I have gone to bbc")]
+        public void GivenIHaveGoneToBbc()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"The Home Page Loads")]
+        public void WhenTheHomePageLoads()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"The Sport Link appears")]
+        public void ThenTheSportLinkAppears()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
